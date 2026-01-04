8eaf672e03b460ba7fccb9ff23cbfe7489e0a9c0
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2026-01-04 14:03:49 +0100

    configure for deployment (pages), update photo paths

--- src/photos.js
+++ src/photos.js
@@ -1,16 +1,16 @@
 const photos = [
-    { id: 2, src: "/ireland/DSC04136.JPG", location: "Ireland", year: 2025, desc: "Cliffs of Moher" },
-    { id: 3, src: "/ireland/DSC03798.JPG", location: "Ireland", year: 2025, desc: "Howth Cliff" },
-    { id: 4, src: "/ireland/DSC04133.JPG", location: "Ireland", year: 2025, desc: "Cliffs of Moher" },
-    // { id: 5, src: "/ireland/DSC04158.JPG", location: "Ireland", year: 2025, desc: "Cliffs of Moher" },
-    { id: 6, src: "/portugal/DSC02534.JPG", location: "Portugal", year: 2025, desc: "Seven Hanging Valleys" },
-    { id: 7, src: "/portugal/DSC02283.JPG", location: "Portugal", year: 2025, desc: "Seven Hanging Valleys" },
-    { id: 8, src: "/portugal/DSC02552.JPG", location: "Portugal", year: 2025, desc: "Seven Hanging Valleys" },
-    { id: 9, src: "/portugal/DSC02615.JPG", location: "Portugal", year: 2025, desc: "Seven Hanging Valleys" },
-    { id: 10, src: "/netherlands/DSC01816.jpg", location: "The Netherlands", year: 2025, desc: "Zaanse Schans" },
-    { id: 11, src: "/netherlands/DSC02088.JPG", location: "The Netherlands", year: 2025, desc: "Utrecht" },
-    { id: 12, src: "/colorado/DSC01205.jpg", location: "Colorado", year: 2025, desc: "Red Rocks Amphitheatre" },
-    { id: 13, src: "/california/IMG_4607.jpg", location: "California", year: 2025, desc: "Newport Beach" },
+    { id: 2, src: "/photofolio/ireland/DSC04136.JPG", location: "Ireland", year: 2025, desc: "Cliffs of Moher" },
+    { id: 3, src: "/photofolio/ireland/DSC03798.JPG", location: "Ireland", year: 2025, desc: "Howth Cliff" },
+    { id: 4, src: "/photofolio/ireland/DSC04133.JPG", location: "Ireland", year: 2025, desc: "Cliffs of Moher" },
+    // { id: 5, src: "/photofolio/ireland/DSC04158.JPG", location: "Ireland", year: 2025, desc: "Cliffs of Moher" },
+    { id: 6, src: "/photofolio/portugal/DSC02534.JPG", location: "Portugal", year: 2025, desc: "Seven Hanging Valleys" },
+    { id: 7, src: "/photofolio/portugal/DSC02283.JPG", location: "Portugal", year: 2025, desc: "Seven Hanging Valleys" },
+    { id: 8, src: "/photofolio/portugal/DSC02552.JPG", location: "Portugal", year: 2025, desc: "Seven Hanging Valleys" },
+    { id: 9, src: "/photofolio/portugal/DSC02615.JPG", location: "Portugal", year: 2025, desc: "Seven Hanging Valleys" },
+    { id: 10, src: "/photofolio/netherlands/DSC01816.jpg", location: "The Netherlands", year: 2025, desc: "Zaanse Schans" },
+    { id: 11, src: "/photofolio/netherlands/DSC02088.JPG", location: "The Netherlands", year: 2025, desc: "Utrecht" },
+    { id: 12, src: "/photofolio/colorado/DSC01205.jpg", location: "Colorado", year: 2025, desc: "Red Rocks Amphitheatre" },
+    { id: 13, src: "/photofolio/california/IMG_4607.jpg", location: "California", year: 2025, desc: "Newport Beach" },
 ];
 
 export default photos;
