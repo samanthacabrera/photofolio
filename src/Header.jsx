cd12e1e71492bca5c9fe92285b969fc34daafc14
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2026-02-20 11:15:36 +0100

    fix Header dropdown and hide link to Map

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ const MENU_ITEMS = [
   { label: "Home", hash: "#home" },
   { label: "About", hash: "#about" },
   { label: "Gallery", hash: "#gallery" },
-  { label: "Map", hash: "#map" },
+  // { label: "Map", hash: "#map" },
 ];
 
 export default function Header() {
