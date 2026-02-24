cadc0d313cf74697a0676566b02002275da04148
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2026-02-24 13:28:47 +0100

    add more photos (first macros)

--- src/photos.js
+++ src/photos.js
@@ -1,22 +1,36 @@
 const photos = [
-    { id: 1, src: "/photofolio/usa/IMG_5073 2.jpg", location: "Colorado", lat: 39.66, lng: -105.2, year: 2025, desc: "Red Rocks Park", category: "landscape", featured: false },
-    { id: 2, src: "/photofolio/usa/IMG_5075.jpg", location: "California", lat: 33.62, lng: -117.94, year: 2025, desc: "Newport Beach", category: "landscape", featured: false },
-    { id: 3, src: "/photofolio/portugal/DSC02283.JPG", location: "Portugal", lat: 37.090, lng: -8.2, year: 2025, desc: "Seven Hanging Valleys", category: "landscape", featured: false },
-    { id: 4, src: "/photofolio/portugal/DSC02552.JPG", location: "Portugal", lat: 37.093, lng: -8.21, year: 2025, desc: "Seven Hanging Valleys", category: "landscape", featured: true },
-    { id: 5, src: "/photofolio/portugal/DSC02615.JPG", location: "Portugal", lat: 37.089, lng: -8.22, year: 2025, desc: "Seven Hanging Valleys", category: "landscape", featured: false },
-    { id: 6, src: "/photofolio/portugal/DSC02534.JPG", location: "Portugal", lat: 37.088, lng: -8.23, year: 2025, desc: "Seven Hanging Valleys", category: "landscape", featured: true },
-    { id: 7, src: "/photofolio/italy/DSC03257.JPG", location: "Italy", lat: 46, lng: 9.293, year: 2025, desc: "Lake Como", category: "landscape", featured: false },
-    { id: 8, src: "/photofolio/italy/DSC03244.JPG", location: "Italy", lat: 45.99, lng: 9.295, year: 2025, desc: "Lake Como", category: "landscape", featured: false },
-    { id: 9, src: "/photofolio/ireland/DSC04136.JPG", location: "Ireland", lat: 53.015, lng: -9.399, year: 2025, desc: "Cliffs of Moher", category: "landscape", featured: true },
-    { id: 10, src: "/photofolio/ireland/DSC03798.JPG", location: "Ireland", lat: 53.4, lng: -6.1, year: 2025, desc: "Howth Cliff", category: "landscape", featured: true },
-    { id: 11, src: "/photofolio/ireland/DSC04133.JPG", location: "Ireland", lat: 53.013, lng: -9.398, year: 2025, desc: "Cliffs of Moher", category: "landscape", featured: false },
-    { id: 12, src: "/photofolio/ireland/DSC04158.JPG", location: "Ireland", lat: 53.014, lng: -9.396, year: 2025, desc: "Cliffs of Moher", category: "landscape", featured: false },
-    { id: 13, src: "/photofolio/norway/DSC00485.JPG", location: "Norway", lat: 69.65, lng: 18.9, year: 2026, desc: "Ersfjordbotn", category: "landscape", featured: false },
-    { id: 14, src: "/photofolio/norway/DSC00474.JPG", location: "Norway", lat: 69.652, lng: 18.913, year: 2026, desc: "Ersfjordbotn", category: "landscape", featured: false },
-    { id: 15, src: "/photofolio/norway/DSC00532.JPG", location: "Norway", lat: 69.654, lng: 18.91, year: 2026, desc: "Ersfjordbotn", category: "landscape", featured: false },
-    { id: 16, src: "/photofolio/norway/DSC00466.JPG", location: "Norway", lat: 69.651, lng: 18.915, year: 2026, desc: "Ersfjordbotn", category: "landscape", featured: false },
-    // ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    { id: 17, src: "/photofolio/architecture/DSC03575.JPG", location: "Ireland", lat: 53.34, lng: -6.26, year: 2025, desc: "Dublin", category: "architecture", featured: false },
+    // LANDSCAPE ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    { id: 1, src: "/photofolio/bahamas/IMG_0323.JPG", location: "Bahamas", lat: 0, lng: 0, year: 2021, desc: "", category: "landscape", featured: false },
+    { id: 2, src: "/photofolio/usa/IMG_5073 2.jpg", location: "Colorado", lat: 39.66, lng: -105.2, year: 2025, desc: "Red Rocks Park", category: "landscape", featured: false },
+    { id: 3, src: "/photofolio/usa/IMG_5075.jpg", location: "California", lat: 33.62, lng: -117.94, year: 2025, desc: "Newport Beach", category: "landscape", featured: false },
+    { id: 4, src: "/photofolio/usa/DSC00910.JPG", location: "California", lat: 33.62, lng: -117.94, year: 2025, desc: "Newport Beach", category: "landscape", featured: false },
+    { id: 5, src: "/photofolio/portugal/DSC02283.JPG", location: "Portugal", lat: 37.090, lng: -8.2, year: 2025, desc: "Seven Hanging Valleys", category: "landscape", featured: false },
+    { id: 6, src: "/photofolio/portugal/DSC02552.JPG", location: "Portugal", lat: 37.093, lng: -8.21, year: 2025, desc: "Seven Hanging Valleys", category: "landscape", featured: true },
+    { id: 7, src: "/photofolio/portugal/DSC02615.JPG", location: "Portugal", lat: 37.089, lng: -8.22, year: 2025, desc: "Seven Hanging Valleys", category: "landscape", featured: false },
+    { id: 8, src: "/photofolio/portugal/DSC02534.JPG", location: "Portugal", lat: 37.088, lng: -8.23, year: 2025, desc: "Seven Hanging Valleys", category: "landscape", featured: true },
+    { id: 9, src: "/photofolio/italy/DSC03257.JPG", location: "Italy", lat: 46, lng: 9.293, year: 2025, desc: "Lake Como", category: "landscape", featured: false },
+    { id: 10, src: "/photofolio/italy/DSC03244.JPG", location: "Italy", lat: 45.99, lng: 9.295, year: 2025, desc: "Lake Como", category: "landscape", featured: false },
+    { id: 11, src: "/photofolio/ireland/DSC04136.JPG", location: "Ireland", lat: 53.015, lng: -9.399, year: 2025, desc: "Cliffs of Moher", category: "landscape", featured: true },
+    { id: 12, src: "/photofolio/ireland/DSC03798.JPG", location: "Ireland", lat: 53.4, lng: -6.1, year: 2025, desc: "Howth Cliff", category: "landscape", featured: true },
+    { id: 13, src: "/photofolio/ireland/DSC04133.JPG", location: "Ireland", lat: 53.013, lng: -9.398, year: 2025, desc: "Cliffs of Moher", category: "landscape", featured: false },
+    { id: 14, src: "/photofolio/ireland/DSC04158.JPG", location: "Ireland", lat: 53.014, lng: -9.396, year: 2025, desc: "Cliffs of Moher", category: "landscape", featured: false },
+    { id: 15, src: "/photofolio/norway/DSC00485.JPG", location: "Norway", lat: 69.65, lng: 18.9, year: 2026, desc: "Ersfjordbotn", category: "landscape", featured: false },
+    { id: 16, src: "/photofolio/norway/DSC00474.JPG", location: "Norway", lat: 69.652, lng: 18.913, year: 2026, desc: "Ersfjordbotn", category: "landscape", featured: false },
+    { id: 17, src: "/photofolio/norway/DSC00532.JPG", location: "Norway", lat: 69.654, lng: 18.91, year: 2026, desc: "Ersfjordbotn", category: "landscape", featured: false },
+    { id: 18, src: "/photofolio/norway/DSC00466.JPG", location: "Norway", lat: 69.651, lng: 18.915, year: 2026, desc: "Ersfjordbotn", category: "landscape", featured: false },
+    { id: 19, src: "/photofolio/norway/DSC00422.JPG", location: "Norway", lat: 69.651, lng: 18.915, year: 2026, desc: "Tromso", category: "landscape", featured: false },
+    // ARCHITECTURE ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    { id: 20, src: "/photofolio/architecture/DSC01273.JPG", location: "The Netherlands", lat: 52.16, lng: 4.48, year: 2025, desc: "Leiden", category: "architecture", featured: false },
+    { id: 21, src: "/photofolio/architecture/DSC03575.JPG", location: "Ireland", lat: 53.34, lng: -6.26, year: 2025, desc: "Dublin", category: "architecture", featured: false },
+    { id: 22, src: "/photofolio/architecture/DSC00750 2.JPG", location: "France", lat: 48.85, lng: 2.35, year: 2026, desc: "Paris", category: "architecture", featured: false },
+    { id: 23, src: "/photofolio/architecture/DSC00779.JPG", location: "France", lat: 48.85, lng: 2.35, year: 2026, desc: "Paris", category: "architecture", featured: false },
+    { id: 24, src: "/photofolio/architecture/DSC00787 2.JPG", location: "France", lat: 48.85, lng: 2.35, year: 2026, desc: "Paris", category: "architecture", featured: false },
+    { id: 25, src: "/photofolio/architecture/DSC00791 2.JPG", location: "France", lat: 48.85, lng: 2.35, year: 2026, desc: "Paris", category: "architecture", featured: false },
+    // MACRO ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    { id: 26, src: "/photofolio/macro/DSC00260.JPG", location: "California", lat: 33.62, lng: -117.94, year: 2025, desc: "Newport Beach", category: "macro", featured: false },
+    { id: 27, src: "/photofolio/macro/DSC00265.JPG", location: "California", lat: 33.62, lng: -117.94, year: 2025, desc: "Newport Beach", category: "macro", featured: false },
+
+
 ];
 
 export default photos;
