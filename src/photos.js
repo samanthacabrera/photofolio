b210ef138f565c714758da6d899e026349ffd973
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2026-04-24 10:52:46 +0200

    update photo order

--- src/photos.js
+++ src/photos.js
@@ -1,51 +1,54 @@
 const photos = [
-    // ARCHITECTURE ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    { id: 21, src: "/photofolio/architecture/DSC01273.JPG", country: "The Netherlands", city: "Leiden", year: 2025, category: "architecture" },
-    { id: 22, src: "/photofolio/architecture/DSC03575.JPG", country: "Ireland", city: "Dublin", year: 2025, category: "architecture" },
-    { id: 23, src: "/photofolio/architecture/DSC00750 2.JPG", country: "France", city: "Paris", year: 2026, category: "architecture" },
-    { id: 24, src: "/photofolio/architecture/DSC00779.JPG", country: "France", city: "Paris", year: 2026, category: "architecture" },
-    { id: 25, src: "/photofolio/architecture/DSC00787 2.JPG", country: "France", city: "Paris", year: 2026, category: "architecture" },
-    { id: 26, src: "/photofolio/architecture/DSC00748.JPG", country: "France", city: "Paris", year: 2026, category: "architecture" },
-    { id: 27, src: "/photofolio/architecture/DSC00791.JPG", country: "France", city: "Paris", year: 2026, category: "architecture" },
-    { id: 28, src: "/photofolio/architecture/DSC01011.JPG", country: "Morocco", city: "Marrakesh", year: 2026, category: "architecture" },
-    { id: 29, src: "/photofolio/architecture/DSC01396.JPG", country: "Morocco", city: "Marrakesh", year: 2026, category: "architecture" },
-    { id: 30, src: "/photofolio/architecture/DSC01353.JPG", country: "Morocco", city: "Marrakesh", year: 2026, category: "architecture" },
-
-
-
-    // LANDSCAPE ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     { id: 1, src: "/photofolio/bahamas/IMG_0323.JPG", country: "Bahamas", city: "Nassau", year: 2021, category: "landscape" },
-    { id: 2, src: "/photofolio/usa/IMG_5073 2.jpg", country: "Colorado", city: "Red Rocks Park", year: 2025, category: "landscape" },
-    { id: 3, src: "/photofolio/usa/IMG_5075.jpg", country: "California", city: "Newport Beach", year: 2025, category: "landscape" },
-    { id: 4, src: "/photofolio/usa/DSC00910.JPG", country: "California", city: "Newport Beach", year: 2025, category: "landscape" },
-    { id: 5, src: "/photofolio/portugal/DSC02283.JPG", country: "Portugal", city: "Seven Hanging Valleys", year: 2025, category: "landscape" },
-    { id: 6, src: "/photofolio/portugal/DSC02552.JPG", country: "Portugal", city: "Seven Hanging Valleys", year: 2025, category: "landscape" },
-    { id: 7, src: "/photofolio/portugal/DSC02615.JPG", country: "Portugal", city: "Seven Hanging Valleys", year: 2025, category: "landscape" },
-    { id: 8, src: "/photofolio/portugal/DSC02534.JPG", country: "Portugal", city: "Seven Hanging Valleys", year: 2025, category: "landscape" },
-    { id: 9, src: "/photofolio/italy/DSC03257.JPG", country: "Italy", city: "Lake Como", year: 2025, category: "landscape" },
-    { id: 10, src: "/photofolio/italy/DSC03244.JPG", country: "Italy", city: "Lake Como", year: 2025, category: "landscape" },
-    { id: 11, src: "/photofolio/ireland/DSC04136.JPG", country: "Ireland", city: "Cliffs of Moher", year: 2025, category: "landscape" },
-    { id: 12, src: "/photofolio/ireland/DSC03798.JPG", country: "Ireland", city: "Howth Cliff", year: 2025, category: "landscape" },
-    { id: 13, src: "/photofolio/ireland/DSC04133.JPG", country: "Ireland", city: "Cliffs of Moher", year: 2025, category: "landscape" },
-    { id: 14, src: "/photofolio/ireland/DSC04158.JPG", country: "Ireland", city: "Cliffs of Moher", year: 2025, category: "landscape" },
+
+    { id: 2, src: "/photofolio/usa/IMG_5075.jpg", country: "California", city: "Newport Beach", year: 2025, category: "landscape" },
+    { id: 3, src: "/photofolio/usa/DSC00910.JPG", country: "California", city: "Newport Beach", year: 2025, category: "landscape" },
+    { id: 4, src: "/photofolio/macro/DSC00260.JPG", country: "California", city: "Newport Beach", year: 2025, category: "macro" },
+    { id: 5, src: "/photofolio/macro/DSC00265.JPG", country: "California", city: "Newport Beach", year: 2025, category: "macro" },
+
+    { id: 6, src: "/photofolio/usa/IMG_5073 2.jpg", country: "Colorado", city: "Red Rocks Park", year: 2025, category: "landscape" },
+
+    { id: 7, src: "/photofolio/portugal/DSC02283.JPG", country: "Portugal", city: "Seven Hanging Valleys", year: 2025, category: "landscape" },
+    { id: 8, src: "/photofolio/portugal/DSC02552.JPG", country: "Portugal", city: "Seven Hanging Valleys", year: 2025, category: "landscape" },
+    { id: 9, src: "/photofolio/portugal/DSC02615.JPG", country: "Portugal", city: "Seven Hanging Valleys", year: 2025, category: "landscape" },
+    { id: 10, src: "/photofolio/portugal/DSC02534.JPG", country: "Portugal", city: "Seven Hanging Valleys", year: 2025, category: "landscape" },
+
+    { id: 11, src: "/photofolio/italy/DSC03257.JPG", country: "Italy", city: "Lake Como", year: 2025, category: "landscape" },
+    { id: 12, src: "/photofolio/italy/DSC03244.JPG", country: "Italy", city: "Lake Como", year: 2025, category: "landscape" },
+
+    { id: 13, src: "/photofolio/architecture/DSC03575.JPG", country: "Ireland", city: "Dublin", year: 2025, category: "architecture" },
+    { id: 14, src: "/photofolio/ireland/DSC04136.JPG", country: "Ireland", city: "Cliffs of Moher", year: 2025, category: "landscape" },
+    { id: 15, src: "/photofolio/ireland/DSC03798.JPG", country: "Ireland", city: "Howth Cliff", year: 2025, category: "landscape" },
+    { id: 16, src: "/photofolio/ireland/DSC04133.JPG", country: "Ireland", city: "Cliffs of Moher", year: 2025, category: "landscape" },
+    { id: 17, src: "/photofolio/ireland/DSC04158.JPG", country: "Ireland", city: "Cliffs of Moher", year: 2025, category: "landscape" },
+
     { id: 18, src: "/photofolio/norway/DSC00532.JPG", country: "Norway", city: "Ersfjordbotn", year: 2026, category: "landscape" },
     { id: 19, src: "/photofolio/norway/DSC00466.JPG", country: "Norway", city: "Ersfjordbotn", year: 2026, category: "landscape" },
-    { id: 17, src: "/photofolio/norway/DSC00422.JPG", country: "Norway", city: "Tromso", year: 2026, category: "landscape" },
-    { id: 20, src: "/photofolio/morocco/DSC01323.JPG", country: "Morocco", city: "Sahara Desert", year: 2026, category: "landscape" },
+    { id: 20, src: "/photofolio/norway/DSC00422.JPG", country: "Norway", city: "Tromso", year: 2026, category: "landscape" },
 
-    // MACRO ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    { id: 30, src: "/photofolio/macro/DSC00260.JPG", country: "California", city: "Newport Beach", year: 2025, category: "macro" },
-    { id: 31, src: "/photofolio/macro/DSC00265.JPG", country: "California", city: "Newport Beach", year: 2025, category: "macro" },
-    { id: 33, src: "/photofolio/macro/DSC01025.JPG", country: "Morocco", city: "Marrakesh", year: 2026, category: "macro" },
-    { id: 35, src: "/photofolio/macro/DSC01043.JPG", country: "Morocco", city: "Marrakesh", year: 2026, category: "macro" },
-    { id: 36, src: "/photofolio/macro/DSC01394.JPG", country: "Morocco", city: "Marrakesh", year: 2026, category: "macro" },
-    { id: 37, src: "/photofolio/macro/DSC01395.JPG", country: "Morocco", city: "Marrakesh", year: 2026, category: "macro" },
-    { id: 38, src: "/photofolio/macro/DSC01672.JPG", country: "The Netherlands", city: "Keukenhof", year: 2026, category: "macro" },
-    { id: 39, src: "/photofolio/macro/DSC01637.JPG", country: "The Netherlands", city: "Keukenhof", year: 2026, category: "macro" },
-    { id: 40, src: "/photofolio/macro/DSC01565.JPG", country: "The Netherlands", city: "Keukenhof", year: 2026, category: "macro" },
-    { id: 41, src: "/photofolio/macro/DSC01604.JPG", country: "The Netherlands", city: "Keukenhof", year: 2026, category: "macro" },
-    { id: 42, src: "/photofolio/macro/DSC01654.JPG", country: "The Netherlands", city: "Keukenhof", year: 2026, category: "macro" },
-    { id: 43, src: "/photofolio/macro/DSC01599.JPG", country: "The Netherlands", city: "Keukenhof", year: 2026, category: "macro" },
+    { id: 21, src: "/photofolio/architecture/DSC00750 2.JPG", country: "France", city: "Paris", year: 2026, category: "architecture" },
+    { id: 22, src: "/photofolio/architecture/DSC00779.JPG", country: "France", city: "Paris", year: 2026, category: "architecture" },
+    { id: 23, src: "/photofolio/architecture/DSC00787 2.JPG", country: "France", city: "Paris", year: 2026, category: "architecture" },
+    { id: 24, src: "/photofolio/architecture/DSC00748.JPG", country: "France", city: "Paris", year: 2026, category: "architecture" },
+    { id: 25, src: "/photofolio/architecture/DSC00791.JPG", country: "France", city: "Paris", year: 2026, category: "architecture" },
+
+    { id: 26, src: "/photofolio/architecture/DSC01011.JPG", country: "Morocco", city: "Marrakesh", year: 2026, category: "architecture" },
+    { id: 27, src: "/photofolio/architecture/DSC01396.JPG", country: "Morocco", city: "Marrakesh", year: 2026, category: "architecture" },
+    { id: 28, src: "/photofolio/architecture/DSC01353.JPG", country: "Morocco", city: "Marrakesh", year: 2026, category: "architecture" },
+    { id: 29, src: "/photofolio/morocco/DSC01323.JPG", country: "Morocco", city: "Sahara Desert", year: 2026, category: "landscape" },
+    { id: 30, src: "/photofolio/macro/DSC01025.JPG", country: "Morocco", city: "Marrakesh", year: 2026, category: "macro" },
+    { id: 31, src: "/photofolio/macro/DSC01043.JPG", country: "Morocco", city: "Marrakesh", year: 2026, category: "macro" },
+    { id: 32, src: "/photofolio/macro/DSC01394.JPG", country: "Morocco", city: "Marrakesh", year: 2026, category: "macro" },
+    { id: 33, src: "/photofolio/macro/DSC01395.JPG", country: "Morocco", city: "Marrakesh", year: 2026, category: "macro" },
+
+    { id: 34, src: "/photofolio/architecture/DSC01273.JPG", country: "The Netherlands", city: "Leiden", year: 2025, category: "architecture" },
+    { id: 35, src: "/photofolio/macro/DSC01672.JPG", country: "The Netherlands", city: "Keukenhof", year: 2026, category: "macro" },
+    { id: 36, src: "/photofolio/macro/DSC01637.JPG", country: "The Netherlands", city: "Keukenhof", year: 2026, category: "macro" },
+    { id: 37, src: "/photofolio/macro/DSC01565.JPG", country: "The Netherlands", city: "Keukenhof", year: 2026, category: "macro" },
+    { id: 38, src: "/photofolio/macro/DSC01604.JPG", country: "The Netherlands", city: "Keukenhof", year: 2026, category: "macro" },
+    { id: 39, src: "/photofolio/macro/DSC01654.JPG", country: "The Netherlands", city: "Keukenhof", year: 2026, category: "macro" },
+    { id: 40, src: "/photofolio/macro/DSC01599.JPG", country: "The Netherlands", city: "Keukenhof", year: 2026, category: "macro" },
+
 ];
 
 export default photos;
